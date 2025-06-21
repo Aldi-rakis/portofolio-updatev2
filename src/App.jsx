944559f1edca8d5a7a225973970e3a8d5573950c
Aldi-rakis <aldirakis12@gmail.com>
2025-06-21 21:36:39 +0700

    scrollbary Y

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,20 @@ import Navbar from './component/Navbar';
 import { motion, useScroll, useSpring } from "framer-motion";
 function App() {
   const [count, setCount] = useState(0)
+ const { scrollYProgress } = useScroll();
+  const scaleY  = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
 
   return (
 
-    <div className="App">
+    <div className="App scrollbar-hide ">
+     
 
     <Navbar />
+     <motion.div className="progress-bar" style={{ scaleY }} />
       <Routes/>
 
     </div>
